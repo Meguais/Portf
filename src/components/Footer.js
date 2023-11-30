bcc00020e6943771c311b852e7a63cb9e7549f33
Sinan BENLİBAŞ <47488328+Meguais@users.noreply.github.com>
2023-11-30 18:22:36 +0300

    Update Footer.js
    f

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -41,16 +41,7 @@ function Footer() {
                 <AiOutlineTwitter />
               </a>
             </li>
-            <li className="social-icons">
-              <a
-                href="https://www.linkedin.com/in/sinan-benlibaş-84590027b"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <FaLinkedinIn />
-              </a>
-            </li>
+
             <li className="social-icons">
               <a
                 href="https://www.instagram.com/meguwsh/"
